d1da6bc798d8524caef846a4c31966e13c74259b
z4k1 <you@example.com>
2026-05-02 18:43:26 +0100

    fix CORS for render

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
   'http://127.0.0.1:5000',
+  'https://meditriage-backend-9p44.onrender.com',
   'capacitor://localhost',
   'http://localhost',
   'https://dividing-lucrative-capacity.ngrok-free.dev',
